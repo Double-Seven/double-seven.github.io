cda7b9419d21d45b5c4d1e54692fd289e9806768
akira77 <395185754@qq.com>
2020-06-27 00:33:47 -0400

    tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: 'UA-171058987-1',
       },
     },
     `gatsby-plugin-sitemap`,
