7add6d161d9c08d2dedcd0ffc0325d0679cf1467
ZiQi <ziqi.li@mail.mcgill.ca>
2020-06-29 19:09:52 -0400

    deployed

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,12 +5,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/.cache/dev-404-page.js"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-blog-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/blog-query.tsx"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-homepage-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/homepage-query.tsx"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-page-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/page-query.tsx"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-post-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/post-query.tsx"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-tag-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/tag-query.tsx"))),
-  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-tags-query-tsx": hot(preferDefault(require("/Users/akira77/Programming/Project/personalWeb/personalBlog/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/tags-query.tsx")))
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-blog-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/blog-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-homepage-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/homepage-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-page-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/page-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-post-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/post-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-tag-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/tag-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-minimal-blog-core-src-templates-tags-query-tsx": hot(preferDefault(require("/Users/akira77/Documents/GitHub/double-seven.github.io/node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/templates/tags-query.tsx")))
 }
 
